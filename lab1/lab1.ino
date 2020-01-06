938671ddec238bd286865f0b4c64be44a55f7cef
IRemsha <meww.mailbox@gmail.com>
2020-01-06 15:09:39 +0500

    corrections

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -8,8 +8,8 @@
 
 #define PIN_BUTTON 4
 
-unsigned long last_time;
-unsigned long blick_time;
+unsigned long start_time;
+unsigned long flash_time;
 
 Button start  = Button(PIN_BUTTON);
 
@@ -23,31 +23,23 @@ void setup()
 
 void loop() 
 {
-    bool blick = false;
+    bool flash = false;
     set_rgb_led(0, 255, 0);
-    if (start.wasPressed())
-    {   
-        last_time = millis();
-         while (1)
-        {
-          unsigned long interval = millis() - last_time;
-            if (interval < 30000)
-            {
-                set_rgb_led(255, 0, 0);
-                if (interval % 5000 == 0)
-                {
-                  blick = true;
-                  blick_time = millis();
-                }
-                else if (blick && (millis() - blick_time < 300)){
-                   set_rgb_led(255, 255, 255);
-                   
-                  }
+    if (start.wasPressed()) {   
+    	start_time = millis();
+	    unsigned long interval = millis() - start_time;
+        if (interval < 30000) {
+            set_rgb_led(255, 0, 0);
+            if (interval % 5000 == 0) {
+              flash = true;
+              flash_time = millis();
+            } else if (flash && (millis() - flash_time < 300)) {
+               set_rgb_led(255, 255, 255);
             }
-            else {
-                set_rgb_led(0, 0, 255);
-            }
-        };
+        } else {
+            set_rgb_led(0, 0, 255);
+        }
+        
         return;
     }
 } 
